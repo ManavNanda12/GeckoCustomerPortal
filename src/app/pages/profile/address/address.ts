8127c877f81406d595673e89eec98c0c654d7eef
DESKTOP-CR240EQ\Evince <manavevince@gmail.com>
2025-12-10 15:28:57 +0530

    feat: confirmation add ons

--- src/app/pages/profile/address/address.ts
+++ src/app/pages/profile/address/address.ts
@@ -6,103 +6,138 @@ import { Common } from '../../../../services/common';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AddEditAddressDialog } from './add-edit-address-dialog/add-edit-address-dialog';
 import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-address',
-  imports: [CommonModule , MatDialogModule],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './address.html',
-  styleUrl: './address.css'
+  styleUrl: './address.css',
 })
 export class Address {
-
   //Common Properties
-  addressList:any;
+  addressList: any;
 
-  constructor(private readonly api:ApiUrlHelper,
-    private readonly spinner:NgxSpinnerService,
-    private readonly common:Common,
-    private readonly dialog:MatDialog,
-    private toastr:ToastrService
-  ){}
+  constructor(
+    private readonly api: ApiUrlHelper,
+    private readonly spinner: NgxSpinnerService,
+    private readonly common: Common,
+    private readonly dialog: MatDialog,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.getAddressList();
   }
 
-  getAddressList(){
+  getAddressList() {
     this.spinner.show();
-    let api = this.api.Address.GetAddressList.replace('{customerId}',localStorage.getItem('CustomerId') || '0');
-    this.common.getData(api).pipe().subscribe({
-      next:(response)=>{
-        if(response.success){
-          this.addressList = response.data;
-        }
-      },
-      error:(error)=>{
-        console.log(error);
-      },
-      complete:()=>{
-        this.spinner.hide();
-      }
-    })
+    let api = this.api.Address.GetAddressList.replace(
+      '{customerId}',
+      localStorage.getItem('CustomerId') || '0'
+    );
+    this.common
+      .getData(api)
+      .pipe()
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+            this.addressList = response.data;
+          }
+        },
+        error: (error) => {
+          console.log(error);
+        },
+        complete: () => {
+          this.spinner.hide();
+        },
+      });
   }
 
-  openAddressDialog(address:any){
-    let dialogRef = this.dialog.open(AddEditAddressDialog,{
-      data:{address},
-      disableClose:true
+  openAddressDialog(address: any) {
+    let dialogRef = this.dialog.open(AddEditAddressDialog, {
+      data: { address },
+      disableClose: true,
     });
-    dialogRef.afterClosed().subscribe((result)=>{
-      if(result){
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
         this.getAddressList();
       }
-    })
+    });
   }
 
-  makeAddressDefault(address:any){
-    if(address.isDefault){
-      return;
-    }
-    let api = this.api.Address.MakeAddressDefault.replace('{addressId}',address.addressId);
-    this.spinner.show();
-    this.common.getData(api).pipe().subscribe({
-      next:(response)=>{
-        if(response.success){
-          this.toastr.success(response.message);
-          this.getAddressList();
-        }
-      },
-      error:(error)=>{
-        console.log(error);
-      },
-      complete:()=>{
-        this.spinner.hide();
+  makeAddressDefault(address: any) {
+    if (address.isDefault) return;
+
+    Swal.fire({
+      title: 'Set as Default Address?',
+      text: 'Do you want to make this your default address?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Set Default',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let api = this.api.Address.MakeAddressDefault.replace(
+          '{addressId}',
+          address.addressId
+        );
+        this.spinner.show();
+
+        this.common.getData(api).subscribe({
+          next: (response) => {
+            if (response.success) {
+              this.toastr.success(response.message);
+              this.getAddressList();
+            }
+          },
+          error: (error) => {
+            console.log(error);
+          },
+          complete: () => {
+            this.spinner.hide();
+          },
+        });
       }
-    })
+    });
   }
 
-  deleteAddress(address:any){
-    if(address.isDefault){
+  deleteAddress(address: any) {
+    if (address.isDefault) {
       this.toastr.error('You cannot delete default address');
       return;
     }
-    let api = this.api.Address.DeleteAddress.replace('{addressId}',address.addressId);
-    this.spinner.show();
-    this.common.deleteData(api).pipe().subscribe({
-      next:(response)=>{
-        if(response.success){
-          this.toastr.success(response.message);
-          this.getAddressList();
-        }
-      },
-      error:(error)=>{
-        console.log(error);
-      },
-      complete:()=>{
-        this.spinner.hide();
+
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Do you really want to delete this address?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Delete',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let api = this.api.Address.DeleteAddress.replace(
+          '{addressId}',
+          address.addressId
+        );
+        this.spinner.show();
+
+        this.common.deleteData(api).subscribe({
+          next: (response) => {
+            if (response.success) {
+              this.toastr.success(response.message);
+              this.getAddressList();
+            }
+          },
+          error: (error) => {
+            console.log(error);
+          },
+          complete: () => {
+            this.spinner.hide();
+          },
+        });
       }
-    })
-
+    });
   }
-
 }
--- src/app/pages/profile/wishlist/wishlist.ts
+++ src/app/pages/profile/wishlist/wishlist.ts
@@ -4,84 +4,108 @@ import { ApiUrlHelper } from '../../../../common/ApiUrlHelper';
 import { Common } from '../../../../services/common';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { gymImages, ProductResponse } from '../../../../common/models/CommonInterfaces';
+import {
+  gymImages,
+  ProductResponse,
+} from '../../../../common/models/CommonInterfaces';
 import { ProductDetail } from '../../products/product-detail/product-detail';
 import { MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-wishlist',
-  imports: [CommonModule,MatIconModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './wishlist.html',
-  styleUrl: './wishlist.css'
+  styleUrl: './wishlist.css',
 })
 export class Wishlist implements OnInit {
+  // Common Properties
+  wishList: any;
+  customerId: any;
 
-    // Common Properties
-    wishList:any;
-    customerId:any;
-  
-    constructor(private readonly api:ApiUrlHelper,
-      private readonly spinner:NgxSpinnerService,
-      private readonly common:Common,
-      private readonly dialog: MatDialog,
-      private readonly toastr: ToastrService){}
+  constructor(
+    private readonly api: ApiUrlHelper,
+    private readonly spinner: NgxSpinnerService,
+    private readonly common: Common,
+    private readonly dialog: MatDialog,
+    private readonly toastr: ToastrService
+  ) {}
 
-    ngOnInit(): void {
-      this.getWishList();
-      this.customerId = localStorage.getItem('CustomerId') || '';
-    }
+  ngOnInit(): void {
+    this.getWishList();
+    this.customerId = localStorage.getItem('CustomerId') || '';
+  }
 
-    getWishList(){
-      this.spinner.show();
-      let api = this.api.Wishlist.GetWishList;
-      this.common.getData(api).pipe().subscribe({
-        next:(response)=>{
-          if(response.success){
+  getWishList() {
+    this.spinner.show();
+    let api = this.api.Wishlist.GetWishList;
+    this.common
+      .getData(api)
+      .pipe()
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
             this.wishList = response.data;
           }
         },
-        error:(error)=>{
+        error: (error) => {
           console.log(error);
         },
-        complete:()=>{
+        complete: () => {
           this.spinner.hide();
-        }
-      })
-    }
-
-     handleImageError(product: ProductResponse) {
-        product.productImage = gymImages[Math.floor(Math.random() * gymImages.length)];
-      }
-    
-
-    openProductDetail(product :ProductResponse){
-      this.dialog.open(ProductDetail, {
-        data: { product: product },
-        maxWidth:'1000px'
+        },
       });
-    }
+  }
 
-  removeFromWishlist(product:ProductResponse){
-    let api = this.api.Wishlist.RemoveWishlistItem;
-    let data = {
-      ProductId: product.productID,
-      CustomerId: this.customerId
-    }
-    this.common.postData(api,data).pipe().subscribe({
-      next: (res) => {
-        if(res.success){
-          this.toastr.success(res.message);
-          this.getWishList();
-        }
-      },
-      error: (err: any) => {
-        this.toastr.error("Failed to remove product from wishlist", "Error");
-      },
-      complete: () => { this.spinner.hide(); }
-  })
-}
+  handleImageError(product: ProductResponse) {
+    product.productImage =
+      gymImages[Math.floor(Math.random() * gymImages.length)];
+  }
 
-    
+  openProductDetail(product: ProductResponse) {
+    this.dialog.open(ProductDetail, {
+      data: { product: product },
+      maxWidth: '1000px',
+    });
+  }
 
+  removeFromWishlist(product: ProductResponse) {
+    Swal.fire({
+      title: 'Remove item from wishlist?',
+      text: 'Do you want to remove item from wishlist?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Remove',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let api = this.api.Wishlist.RemoveWishlistItem;
+        let data = {
+          ProductId: product.productID,
+          CustomerId: this.customerId,
+        };
+        this.common
+          .postData(api, data)
+          .pipe()
+          .subscribe({
+            next: (res) => {
+              if (res.success) {
+                this.toastr.success(res.message);
+                this.getWishList();
+              }
+            },
+            error: (err: any) => {
+              this.toastr.error(
+                'Failed to remove product from wishlist',
+                'Error'
+              );
+            },
+            complete: () => {
+              this.spinner.hide();
+            },
+          });
+      }
+    });
+  }
 }
